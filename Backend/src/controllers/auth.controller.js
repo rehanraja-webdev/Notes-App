5ee8a789641a2b4f7e63f88c460fb8cc19245fb8
Rehan Raja <rehanraja.dev@gmail.com>
2026-07-10 00:55:39 +0530

    Fix the naming convention

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -3,7 +3,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
-import asyncHandler from "../utils/AsyncHandler.js";
+import asyncHandler from "../utils/asyncHandler.js";
 
 const Register = asyncHandler(async (req, res) => {
   const { fullname, username, email, password, gender, dob } = req.body;
--- Backend/src/controllers/notes.controller.js
+++ Backend/src/controllers/notes.controller.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
-import asyncHandler from "../utils/AsyncHandler.js";
+import asyncHandler from "../utils/asyncHandler.js";
 
 const getUserNotes = asyncHandler(async (req, res) => {
   const user = req.user;
