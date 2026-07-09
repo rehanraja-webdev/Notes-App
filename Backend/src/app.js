33c4a504941fe7f982c145a168ea5b1fec67ac11
Rehan Raja <rehanraja.dev@gmail.com>
2026-07-10 02:36:45 +0530

    Add logo and move cors to the top

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,19 +1,19 @@
 import express from "express";
+import cors from "cors";
 import authRouter from "./routes/auth.routes.js";
 import notesRoutes from "./routes/notes.routes.js";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 
 const app = express();
-app.use(express.json());
-app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://inknotes.netlify.app/"],
+    origin: ["http://localhost:5173", "https://inknotes.netlify.app"],
     credentials: true,
   }),
 );
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 app.use("/api/notes", notesRoutes);
